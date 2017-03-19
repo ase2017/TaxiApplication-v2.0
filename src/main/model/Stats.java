2b5eb0ed393c5ffa31bdb23d89d32d3a95e9afef
jd57hw <jd57@hw.ac.uk>
2017-03-19 02:18:04 +0100

    added George Chiotis's work on charts

--- src/main/model/Stats.java
+++ src/main/model/Stats.java
@@ -1,7 +1,5 @@
 package main.model;
 
-import main.Main;
-
 /**
  * Class that generates stats
  */
@@ -21,21 +19,143 @@ public class Stats {
      */
     public int getAverageNumberOfPassengersPerWindow(){
 
-        int res = 0;
+        int passPerWindow = 0;
 
         if(windows != null && windows.length > 0 ){
             for(Window window : windows) {
                 if(window != null){
-                    res = window.getTotalNumberOfPassengersServed();
+                    passPerWindow += window.getTotalNumberOfPassengersServed();
                 }
             }
 
-            res /= windows.length;
+            passPerWindow /= windows.length;
 
         }
 
-        return res;
+        return passPerWindow;
     }
+
+
+
+    public int getAverageTaxisPerWindow() {
+
+        int taxisPerWindow = 0;
+
+        if(windows != null && windows.length > 0 ){
+
+            for(int i=0; i<windows.length; i++) {
+                if(windows[i] != null){
+                    taxisPerWindow += getNumberOfTaxisThisWindow(i);
+                }
+            }
+
+            taxisPerWindow /= windows.length;
+
+        }
+
+        return taxisPerWindow;
+    }
+
+
+    public int getAverageGroupsPerWindow() {
+
+        int groupsPerWindow = 0;
+
+        if(windows != null && windows.length > 0 ){
+
+            for(int i=0; i<windows.length; i++) {
+                if(windows[i] != null){
+                    groupsPerWindow +=getNumberOfGroupsThisWindow(i);
+                }
+            }
+
+            groupsPerWindow /= windows.length;
+
+        }
+
+        return groupsPerWindow;
+    }
+
+
+
+    public long getAverageWorkingTimePerWindow() {
+
+        long avgTime = 0;
+
+        if(windows != null && windows.length > 0 ){
+
+            for(int i=0; i<windows.length; i++) {
+                if(windows[i] != null){
+                    avgTime += getWorkingTimeThisWindow(i);
+                }
+            }
+
+            avgTime /= windows.length;
+
+        }
+
+        return avgTime;
+
+    }
+
+
+    public int getNumberOfPassengerThisWindow( int windowIndex ) {
+
+
+        if( windows != null && windowIndex < windows.length )
+            return windows[windowIndex].getTotalNumberOfPassengersServed();
+        else
+            return -1;
+
+    }
+
+
+    public int getNumberOfTaxisThisWindow( int windowIndex ) {
+
+
+        if ( windows != null && windowIndex < windows.length )
+            return windows[windowIndex].getTotalNumberOfAllocatedTaxis();
+        else
+            return -1;
+
+    }
+
+
+    public int getNumberOfGroupsThisWindow( int windowIndex ) {
+
+
+        if ( windows != null && windowIndex < windows.length )
+            return windows[windowIndex].getTotalNumberOfAllocatedTaxis();
+        else
+            return -1;
+
+    }
+
+
+
+    public long getWorkingTimeThisWindow( int windowIndex ) {
+
+
+        if ( windows != null && windowIndex < windows.length )
+            return ( windows[windowIndex].getWorkingEndTime() - windows[windowIndex].getWorkingStartTime() );
+        else
+            return -1;
+    }
+
+
+
+    public void printIndividualWindowStats() {
+
+        for(int i=0; i<windows.length; i++) {
+
+            System.out.println( "Window " + i + ": " +
+                    " Working Time: " + getWorkingTimeThisWindow(i) + " NumOfGroups: " + getNumberOfGroupsThisWindow(i) +
+                     " NumOfTaxis: " + getNumberOfTaxisThisWindow(i) + " NumOfPassengers: " + getNumberOfPassengerThisWindow(i) );
+        }
+
+    }
+
+
 
 
 }
